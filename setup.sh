21dcb3e85ddce5e86dcaf36e04cda510a5b4fb0e
marsp0 <martin.n.spasov@gmail.com>
2023-12-27 18:39:24 +0200

    replacing sublime with focus

--- setup.sh
+++ setup.sh
@@ -56,9 +56,7 @@ cp /home/martin/Downloads/dot-files/surf/build/bin/WebKitNetworkProcess /usr/loc
 cp /home/martin/Downloads/dot-files/surf/build/bin/WebKitWebProcess /usr/local/libexec/webkit2gtk-4.1
 
 # echo "----------------------------------------"
-# echo "------- Installing Sublime Text --------"
+# echo "--------- Installing focus -------------"
 # echo "----------------------------------------"
-# wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
-# echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-# sudo apt-get update
-# sudo apt-get install sublime-text --no-install-recommends
+mkdir /home/martin/.config
+cp /home/martin/Downloads/dot-files/focus/focus /usr/bin/
