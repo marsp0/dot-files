4877e4c6dc002a3881055c21831301be6ec61ef3
marsp0 <martin.n.spasov@gmail.com>
2023-12-27 18:37:11 +0200

    Revert "replacing surf with w3m"
    This reverts commit 8321ac3204d86f98329e8542fab89a709cb778e3.

--- setup.sh
+++ setup.sh
@@ -20,16 +20,45 @@ mkdir /home/martin/.gdbinit.d
 cp /home/martin/Downloads/dot-files/gdb-dashboard/init /home/martin/.gdbinit.d/
 
 echo "----------------------------------------"
-echo "------------ Installing dwm ------------"
+echo "-------- Installing dwm + surf ---------"
 echo "----------------------------------------"
+sudo apt install --no-install-recommends dmenu \
+                                         libgcr-ui-3-1 \
+                                         libxslt1.1 \
+                                         libopengl0 \
+                                         libharfbuzz-icu0 \
+                                         libopenjp2-7 \
+                                         libwebpdemux2 \
+                                         libsoup-3.0-0 \
+                                         libsecret-1-0 \
+                                         libhyphen0
+mkdir /usr/local/lib
+mkdir /usr/local/libexec
+mkdir /usr/local/libexec/webkit2gtk-4.1
+
 cp /home/martin/Downloads/dot-files/dwm/dwm /usr/bin/
 
 cp /home/martin/Downloads/dot-files/.Xresources /home/martin/.Xresources
 cp /home/martin/Downloads/dot-files/.xinitrc /home/martin/.xinitrc
 cp /home/martin/Downloads/dot-files/sensors.sh /home/martin/Documents
 cp /home/martin/Downloads/dot-files/clear.sh /home/martin/Documents
+cp /home/martin/Downloads/dot-files/surf.desktop /usr/share/applications/
 
-echo "----------------------------------------"
-echo "------------ Installing w3m ------------"
-echo "----------------------------------------"
-sudo apt install --no-install-recommends w3m
+cp /home/martin/Downloads/dot-files/surf/bin/surf /usr/bin
+cp -r /home/martin/Downloads/dot-files/surf/lib/surf /usr/local/lib
+cp -r /home/martin/Downloads/dot-files/surf/lib/webkit2gtk-4.1 /usr/local/lib
+cp /home/martin/Downloads/dot-files/surf/lib/lib* /usr/lib
+ln -s /usr/lib/libwebkit2gtk-4.1.so /usr/lib/libwebkit2gtk-4.1.so.0
+ln -s /usr/lib/libwebkit2gtk-4.1.so /usr/lib/libwebkit2gtk-4.1.so.0.2.8
+ln -s /usr/lib/libjavascriptcoregtk-4.1.so /usr/lib/libjavascriptcoregtk-4.1.so.0
+ln -s /usr/lib/libjavascriptcoregtk-4.1.so /usr/lib/libjavascriptcoregtk-4.1.so.0.2.8
+cp /home/martin/Downloads/dot-files/surf/build/bin/WebKitNetworkProcess /usr/local/libexec/webkit2gtk-4.1
+cp /home/martin/Downloads/dot-files/surf/build/bin/WebKitWebProcess /usr/local/libexec/webkit2gtk-4.1
+
+# echo "----------------------------------------"
+# echo "------- Installing Sublime Text --------"
+# echo "----------------------------------------"
+# wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
+# echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+# sudo apt-get update
+# sudo apt-get install sublime-text --no-install-recommends
